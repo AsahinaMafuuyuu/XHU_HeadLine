a4ff4fe5637fa08e77862ee1fa20c48ead38b914
yumoni <3336873725@qq.com>
2025-11-14 16:45:38 +0800

    进度1.4 搭建springboot项目

---
+++ back-end/XHU_HeadLine/src/main/java/com/xhu/headline_server/XhuHeadLineApplication.java
@@ -0,0 +1,13 @@
+package com.xhu.headline_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhuHeadLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhuHeadLineApplication.class, args);
+    }
+
+}
---
+++ back-end/XHU_HeadLine/src/test/java/com/xhu/headline_server/XhuHeadLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhu.headline_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XhuHeadLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
